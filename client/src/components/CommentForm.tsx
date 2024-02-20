d8787504d0b4595078d2dcc53ea8d7181f6ac04f
Aleksandr <ledich19@gmail.com>
2024-02-20 10:45:13 +0200

    feat: add file and ing loading

--- client/src/components/CommentForm.tsx
+++ client/src/components/CommentForm.tsx
@@ -1,9 +1,17 @@
-import { useEffect } from "react";
-import { Button, Modal, Stack, TextField } from "@mui/material";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+import {
+  Box,
+  Button,
+  Modal,
+  Stack,
+  TextField,
+  Tooltip,
+} from "@mui/material";
 import ClearIcon from "@mui/icons-material/Clear";
 import { CloseBtn, ModalPaper, StyledTextarea } from "./CommentForm.styled";
 import useInput from "../hooks/useInput";
 import ErrorMessage from "./ErrorMessage";
+import { AddPhotoAlternate, AttachFile, Cancel } from "@mui/icons-material";
 
 interface IProps {
   isOpen: boolean;
@@ -11,6 +19,10 @@ interface IProps {
 }
 
 const CommentForm = ({ isOpen, handleModal }: IProps) => {
+  const [selectedImg, setSelectedImg] = useState<File | null>(null);
+  const inputImgRef = useRef<HTMLInputElement>(null);
+  const [selectedTxt, setSelectedTxt] = useState<File | null>(null);
+  const inputTxtRef = useRef<HTMLInputElement>(null);
   const nameInput = useInput("", {
     isEmpty: false,
     minLength: 3,
@@ -41,6 +53,26 @@ const CommentForm = ({ isOpen, handleModal }: IProps) => {
     throw new Error("---need make handleAddComment---");
   };
 
+  const handleSelectImg = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    setSelectedImg(e.target.files[0]);
+  };
+  const handleCancelImg = () => {
+    setSelectedImg(null);
+    if (inputImgRef.current) {
+      inputImgRef.current.value = "";
+    }
+  };
+  const handleSelectTxt = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    setSelectedTxt(e.target.files[0]);
+  };
+  const handleCancelTxt = () => {
+    setSelectedImg(null);
+    if (inputTxtRef.current) {
+      inputTxtRef.current.value = "";
+    }
+  };
   return (
     <Modal
       open={isOpen ? true : false}
@@ -99,6 +131,73 @@ const CommentForm = ({ isOpen, handleModal }: IProps) => {
             <ErrorMessage error={commentInput.error} />
           )}
         </Stack>
+        <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+          <Tooltip title="Додати картинку">
+            <label style={{ height: "100%", margin: 0 }} htmlFor="imageInput">
+              <input
+                ref={inputImgRef}
+                type="file"
+                id="imageInput"
+                accept="image/jpeg, image/gif, image/png"
+                style={{ display: "none" }}
+                onChange={handleSelectImg}
+              />
+
+              {!selectedImg && (
+                <Button
+                  component="span"
+                  style={{ height: "100%", margin: 0 }}
+                  aria-label="load img"
+                >
+                  <AddPhotoAlternate />
+                </Button>
+              )}
+            </label>
+          </Tooltip>
+          {selectedImg && (
+            <Button
+              component="span"
+              style={{ height: "100%", margin: 0 }}
+              onClick={handleCancelImg}
+              aria-label="cancel"
+            >
+              <Cancel />
+            </Button>
+          )}
+
+          <Tooltip title="add txt file">
+            <label style={{ height: "100%", margin: 0 }} htmlFor="textInput">
+              <input
+                ref={inputTxtRef}
+                type="file"
+                id="textInput"
+                accept=".txt"
+                style={{ display: "none" }}
+                onChange={handleSelectTxt}
+              />
+
+              {!selectedTxt && (
+                <Button
+                  component="span"
+                  style={{ height: "100%", margin: 0 }}
+                  aria-label="load file"
+                >
+                  <AttachFile />
+                </Button>
+              )}
+            </label>
+          </Tooltip>
+          {selectedTxt && (
+            <Button
+              component="span"
+              style={{ height: "100%", margin: 0 }}
+              aria-label="cancel"
+              onClick={handleCancelTxt}
+            >
+              <Cancel />
+            </Button>
+          )}
+        </Box>
         <Button onClick={handleAddComment}>Add comment</Button>
       </ModalPaper>
     </Modal>
